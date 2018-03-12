2454d420aa18a0b7bc9aeb4b84401d89347f6b7f
Anna Yang <yanganna68@gmail.com>
2018-03-11 22:07:13 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-// Code goes here
+import React from 'react';
+
+import { message } from './components/foo';
+
+import fruit from './fruits';
+console.log(fruit.apple);
+
+import { apple } from './fruits';
+console.log(apple);
